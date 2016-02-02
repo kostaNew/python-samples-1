1e648752b390c285b2fc45cf91ffdf768b710398
Konstantin Ushenin <kostaNew@gmail.com>
2016-02-02 13:01:30 +0500

    Модель Алиева-Панфилова из статьи 1996 года

--- aliev-panfilov/test.py
+++ aliev-panfilov/test.py
@@ -1,31 +1,41 @@
 # coding=utf-8
 ############################################################
-# Это пример решения уравнения Алиева-Панфилова в точке.
+# Это пример решения уравнения Алиева-Панфилова (1996) в точке.
 # С помошью odeint
 ############################################################
 
-import numpy as np                  # Модуль для быстрой работы с массивами
-import matplotlib.pyplot as plt     # Модуль для построения графиков
+import numpy as np  # Модуль для быстрой работы с массивами
+import matplotlib.pyplot as plt  # Модуль для построения графиков
 from scipy.integrate import odeint  # Библиотека для интеграции
 
+
 # Тут задаются уравнения
-def construct_model(k=8, a=0.03):
-    def eps_fun(a, u):
-        return 1 if u < a else 0.1
+def construct_model(k=8, a=0.15, eps_0=0.002, mu_1=0.2, mu_2=0.3):
+    def eps_fun(u, v):
+        return eps_0 + mu_1*v/(u+mu_2)
 
     def fun(y, t):
         u, v = y
-        eps = eps_fun(a, u)
-        derivs = [k * u * (1 - u) * (u - a) - u * v,
-                  eps * (k * u - v)]
+        eps = eps_fun(u,v)
+        derivs = [-k*u*(u-a)*(u-1)-u*v,
+                  eps*(-v-k*u*(u-a-1))]
         return derivs
 
     return fun
 
 
+# Тут функции пересчета параметров в физиологические.
+# Что бы понять,смотрите статью
+def u_to_E(u):
+    return 100 * u - 80
+
+
+def t_to_t(t):
+    return 12.9 * t
+
+
 # Тут рисуется картинка
 def vizualize(t, psoln):
-
     fig = plt.figure(1, figsize=(8, 8))
 
     # График "быстрого" тока. Переменная u
@@ -46,6 +56,15 @@ def vizualize(t, psoln):
     ax3.set_xlabel('u')
     ax3.set_ylabel('v')
 
+    # График потенциала в норме
+    E = u_to_E(psoln[:, 0])
+    t_ = t_to_t(t)
+
+    ax3 = fig.add_subplot(224)
+    ax3.plot(t_, E)
+    ax3.set_xlabel('time[ms]')
+    ax3.set_ylabel('E[mV]')
+
     plt.show()
 
 
@@ -53,20 +72,21 @@ def vizualize(t, psoln):
 def main():
     # Посроение сетки
     left_bound = 0.
-    right_bound = 20.
-    discretization = (right_bound - left_bound) * 100.
+    right_bound = 40.
+    discretization = (right_bound - left_bound) * 1000
     t = np.linspace(left_bound, right_bound, discretization)
 
     # Задание системы с помошью функции
     fun = construct_model()
 
     # Начальные условия. Задача Коши
-    y0 = (0.08, 0)
+    y0 = (0.17, 0)
 
     # Вызов солвера
     psoln = odeint(fun, y0, t)
 
     vizualize(t, psoln)
+
 
 if __name__ == "__main__":
     main()
