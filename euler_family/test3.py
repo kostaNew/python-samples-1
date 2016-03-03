93dc53c77a6ebb1eee65f6303e829e196caec7c6
Konstantin Ushenin <kostaNew@gmail.com>
2016-03-04 02:55:04 +0500

    Пила

--- euler_family/test3.py
+++ euler_family/test3.py
@@ -53,16 +53,40 @@ def euler_method(grid, fun, init):
 
     return x_series, y_series
 
+# Уточненный метод Эйлера. Решает уравнения для 1 переменной. Сигнатура: Сетка, функция, начальное значение x
+def euler_method_better(grid, fun, init):
+    x_series = np.zeros(len(grid))
+    y_series = np.zeros(len(grid))
+
+    # В явном методе Эйлера 1- го порядка точности нам не нужны начальные шаги. Мы используем только начальное значение.
+    x_series[0] = init[0]
+    y_series[0] = init[1]
+
+    # Основной цикл
+    for i in xrange(len(grid)-1):
+        h = grid[i+1] - grid[i]             #Так как сетка может быть переменной, лучше узнавать шаг каждый раз
+        x,y = fun((x_series[i], y_series[i]), grid[i])
+
+        x_series_half = x_series[i] + (h/2)*x
+        y_series_half = y_series[i] + (h/2)*y
+
+        x_,y_ = fun((x_series_half, y_series_half), grid[i] + (h/2))
+
+        x_series[i+1] =  x_series[i] + h*x_
+        y_series[i+1] =  y_series[i] + h*y_
+
+    return x_series, y_series
 
 
 # Тут рисуется картинка
-def vizualize(t, psoln, euler_t, euler_x, euler_y):
+def vizualize(t, psoln, euler_t, euler_x, euler_y, euler_better_t, euler_better_x, euler_better_y):
     fig = plt.figure(1, figsize=(8, 8))
 
     # График "быстрого" тока. Переменная u
     ax1 = fig.add_subplot(221)
     ax1.plot(t, psoln[:, 0], color='black')
     ax1.plot(euler_t, euler_x, color='red')
+    ax1.plot(euler_better_t, euler_better_x, color='green')
     ax1.set_xlabel('time')
     ax1.set_ylabel('u')
 
@@ -70,6 +94,7 @@ def vizualize(t, psoln, euler_t, euler_x, euler_y):
     ax2 = fig.add_subplot(222)
     ax2.plot(t, psoln[:, 1], color='black')
     ax2.plot(euler_t, euler_y, color='red')
+    ax2.plot(euler_better_t, euler_better_y, color='green')
     ax2.set_xlabel('time')
     ax2.set_ylabel('v')
 
@@ -77,6 +102,7 @@ def vizualize(t, psoln, euler_t, euler_x, euler_y):
     ax3 = fig.add_subplot(223)
     ax3.plot(psoln[:, 0], psoln[:, 1], color='black')
     ax3.plot(euler_x, euler_y, color='red')
+    ax3.plot(euler_better_x, euler_better_y, color='green')
     ax3.set_xlabel('u')
     ax3.set_ylabel('v')
 
@@ -86,10 +112,13 @@ def vizualize(t, psoln, euler_t, euler_x, euler_y):
 
     E_euler = u_to_E(euler_x)
     t_euler = t_to_t(euler_t)
+    E_euler_ = u_to_E(euler_better_x)
+    t_euler_ = t_to_t(euler_better_t)
 
     ax4 = fig.add_subplot(224)
     ax4.plot(t_, E, color='black')
     ax4.plot(t_euler, E_euler, color='red')
+    ax4.plot(t_euler_, E_euler_, color='green')
     ax4.set_xlabel('time[ms]')
     ax4.set_ylabel('E[mV]')
 
@@ -102,7 +131,7 @@ def main():
     left_bound = 0.
     right_bound = 40.
     discretization = (right_bound - left_bound) * 1000
-    discretization2 = (right_bound - left_bound) * 10
+    discretization2 = (right_bound - left_bound) * 3
     t = np.linspace(left_bound, right_bound, discretization)
 
     # Задание системы с помошью функции
@@ -117,8 +146,9 @@ def main():
     #
     grid = np.linspace(left_bound, right_bound, discretization2)
     x_series, y_series = euler_method(grid, fun, y0)
+    x_series_, y_series_ = euler_method_better(grid, fun, y0)
 
-    vizualize(t, psoln, grid, x_series, y_series)
+    vizualize(t, psoln, grid, x_series, y_series, grid, x_series_, y_series_)
 
 
 if __name__ == "__main__":
