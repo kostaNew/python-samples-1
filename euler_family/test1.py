c8e6b794908daaf6e7789cfea95b5521d02c4036
Konstantin Ushenin <kostaNew@gmail.com>
2016-03-04 01:35:03 +0500

    Неявный метод Эйлера с решением через минимизацию

--- euler_family/test1.py
+++ euler_family/test1.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import fsolve
 
 # Такая реализация будет работать для уравнения с 1 переменной.
 # Производная по t
@@ -16,10 +17,12 @@ def euler_method(grid, fun, init_x):
 
     # Основной цикл
     for i in xrange(len(grid)-1):
-        h = grid[i+1] - grid[i]             #Так как сетка может быть переменной, лучше узнавать шаг каждый раз
+        h = grid[i+1] - grid[i]
         x_series[i+1] =  x_series[i] + h*fun(grid[i], x_series[i])
 
     return x_series
+
+
 
 # Уточненный метод Эйлера. Решает уравнения для 1 переменной. Сигнатура: Сетка, функция, начальное значение x
 def euler_method_better(grid, fun, init_x):
@@ -36,13 +39,37 @@ def euler_method_better(grid, fun, init_x):
 
     return x_series
 
+
+# Неявный метод Эйлера. Решает уравнения для 1 переменной. Сигнатура: Сетка, функция, начальное значение x
+def euler_method_explicit(grid, fun, init_x):
+    x_series = np.zeros(len(grid))
+    x_series[0] = init_x
+
+
+    def euler_construct_min_problem(fun, t_ip1, x_i, h):
+        def ret(x_ip1):
+            return fun(t_ip1, x_ip1) - (x_ip1 - x_i)/h
+
+        return ret
+
+    # Основной цикл
+    for i in xrange(len(grid)-1):
+
+        h = grid[i+1] - grid[i]
+
+        # Решаем нелинейное уравнение, сведя его к задаче минимизации
+        min_problem = euler_construct_min_problem(fun, grid[i+1], x_series[i] , h)
+        x_series[i+1] = fsolve(min_problem, x_series[i])
+
+    return x_series
+
 # Тут вызывается солвер
 def main():
 
     def sample(t, y):
         return y
 
-    N = 3
+    N = 10
 
     grid = np.linspace(0, 5, 5*N)
 
@@ -50,6 +77,7 @@ def main():
 
     x_series = euler_method(grid, sample, init_x)
     x_series_2 = euler_method_better(grid, sample, init_x)
+    x_series_3 = euler_method_explicit(grid, sample, init_x)
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -60,6 +88,7 @@ def main():
     ax.plot(t_series, x_series_exact, linewidth=1, color='grey')
     ax.plot(grid, x_series, linewidth=1, color='green')
     ax.plot(grid, x_series_2, linewidth=1, color='red')
+    ax.plot(grid, x_series_3, linewidth=1, color='blue')
     plt.show()
 
 
